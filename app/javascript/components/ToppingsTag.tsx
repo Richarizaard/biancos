3086a586d4eb0a77a20d1fa49d4880c5817df45b
Richard Lee <richardlee1357@gmail.com>
2023-05-10 08:24:21 -0700

    gem updates

--- app/javascript/components/ToppingsTag.tsx
+++ app/javascript/components/ToppingsTag.tsx
@@ -19,8 +19,16 @@ const ToppingsTag = ({
     if (!isEditOrCreate) setOpenDropdown(false)
   }, [isEditOrCreate])
 
-  const handleAddTag = async (topping: Topping) => {
-    const combinedToppings = [...toppings, topping]
+  const handleAddTag = (addedTopping: Topping) => {
+    const combinedToppings = [...toppings, addedTopping]
+    setToppings(combinedToppings)
+    setOpenDropdown(false)
+  }
+
+  const handleRemoveTag = (removedTopping: Topping) => {
+    const combinedToppings = toppings.filter(
+      (topping) => topping.id !== removedTopping.id
+    )
     setToppings(combinedToppings)
     setOpenDropdown(false)
   }
@@ -33,24 +41,33 @@ const ToppingsTag = ({
     <div className="w-full flex flex-wrap justify-start py-2">
       {toppings.map((topping) => {
         return (
-          <div
-            className="border-2 px-1 m-1 border-bianco-salmon text-bianco-red rounded-lg"
-            key={topping.id}
-          >
-            {topping.name}
+          <div className="border-2 px-1 m-1 border-bianco-salmon text-bianco-red rounded-lg flex gap-1">
+            <div className="" key={topping.id}>
+              {topping.name}
+            </div>
+            {isEditOrCreate && (
+              <div
+                className="cursor-pointer"
+                onClick={() => handleRemoveTag(topping)}
+              >
+                x
+              </div>
+            )}
           </div>
         )
       })}
       {isEditOrCreate && !openDropdown && (
-        <div
-          className="px-2 cursor-pointer flex items-center"
-          onClick={() => setOpenDropdown(true)}
-        >
-          +
+        <div className="relative flex items-center">
+          <div
+            className="px-2 cursor-pointer flex items-center"
+            onClick={() => setOpenDropdown(true)}
+          >
+            +
+          </div>
         </div>
       )}
       {openDropdown && (
-        <div className="relative py-1">
+        <div className="relative py-1 px-2">
           <div className="h-[250px] overflow-scroll border-2 p-2 border-bianco-salmon text-bianco-red rounded-lg bg-white flex flex-col fixed">
             {filteredToppings?.map((topping) => {
               return (
